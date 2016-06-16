4ea06acb0ff1998fd824a5ed66f54823216f4264
alexandercrosson <alexcrosson@gmail.com>
2016-06-15 22:50:36 -0700

    Removing old

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# braise
+# Braise
--- backend/braise_api/controllers/health.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource
-
-class Health(Resource):
-    def get(self):
-        return {'success': True, 'message': '', 'data': None}
-    def post(self):
-        pass
